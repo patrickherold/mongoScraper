f35970a9aff91dd8c9ab60f1fcc360222f20c96f
patrickherold <patrick@herold.org>
2019-04-25 15:36:18 -0400

    update for heroku mongo lab

--- server.js
+++ server.js
@@ -36,8 +36,10 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/scraper", { useNewUrlParser: true });
+// Connect to the Mongo DB
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraper";
 
+mongoose.connect(MONGODB_URI);
 // Routes
 
 app.get('/', function (req, res) {
